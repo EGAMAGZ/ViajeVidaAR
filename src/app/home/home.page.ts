71672b3b6f756ca61336c42d4ef09fbfde6b5c7b
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-09-18 11:03:56 -0600

    feat: Creada logica basica para template de AR

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ArLauncherPage } from '../components/ar-launcher/ar-launcher.page';
 import { Camera } from "@capacitor/camera";
-import { SharedModule } from '../shared/modules/shared/shared.module';
+import { generateARTemplate } from '@/app/shared/utils/template';
+import { environment } from "@/environments/environment";
+import { SharedModule } from '@/app/shared/modules/shared/shared.module';
 
 @Component({
   selector: 'app-home',
