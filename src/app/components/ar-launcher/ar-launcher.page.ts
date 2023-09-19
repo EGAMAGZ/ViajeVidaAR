6487c247584285a5172c301d19d74d7a2e095c69
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-09-18 18:26:58 -0600

    feat: Funcionalidad para generar html apartir de template

--- src/app/components/ar-launcher/ar-launcher.page.ts
+++ src/app/components/ar-launcher/ar-launcher.page.ts
@@ -1,27 +1,36 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule, ModalController } from '@ionic/angular';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ModalController } from '@ionic/angular';
+import { SharedModule } from '@/app/shared/modules/shared/shared.module';
+import { generateArTemplate } from '@/app/shared/utils/template';
 
 @Component({
   selector: 'app-ar-launcher',
   templateUrl: './ar-launcher.page.html',
   styleUrls: ['./ar-launcher.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [SharedModule],
   encapsulation: ViewEncapsulation.None
 })
 export class ArLauncherPage implements OnInit {
 
+  iframeContent: SafeHtml
+
   constructor(
-    private modalController: ModalController
-  ) { }
+    private modalController: ModalController,
+    private domSanitizer: DomSanitizer
+  ) {
+    const a = generateArTemplate({ debug: false, vrModeUI: false })
+    console.log()
+
+    this.iframeContent = domSanitizer.bypassSecurityTrustHtml(a)
+  }
 
   ngOnInit() {
   }
 
 
-  close(){
+  close() {
     this.modalController.dismiss();
   }
 }
---
+++ src/app/shared/utils/template.ts
@@ -0,0 +1,33 @@
+import { renderString, configure } from "nunjucks";
+
+configure(
+  {
+    autoescape: false
+  }
+)
+
+const njkTemplate = `
+<!DOCTYPE html>
+<html>
+<script src="https://aframe.io/releases/0.6.1/aframe.min.js"></script>
+<script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.5.0/aframe/build/aframe-ar.js"></script>
+
+<body style="margin: 0px; overflow: hidden;">
+  <a-scene embedded arjs="debugUIEnabled: {{ debug }};" vr-mode-ui="enabled: {{ vrModeUI }};">
+    <a-marker preset="hiro">
+      <a-image src="./../bunny.png" rotation="0 0 0" width="3" height="4"></a-image>
+    </a-marker>
+    <a-entity camera></a-entity>
+  </a-scene>
+</body>
+
+</html>`;
+
+type ArTemplateParams = {
+  debug: boolean;
+  vrModeUI: boolean;
+}
+
+export function generateArTemplate(params: ArTemplateParams): string {
+  return renderString(njkTemplate, params);
+}
