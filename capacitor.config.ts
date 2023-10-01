bd4cdbdb88be17e155f44be549add50d3c951ca1
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-10-01 17:21:15 -0600

    feat: agregada splashscreen y logo de app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,31 @@
+/// <reference types="@capacitor/splash-screen" />
+
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.patrones.viajevidaar',
-  appName: 'ViajeVidaAR',
+  appName: 'Viaje de la Vida AR',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: '#ffffffff',
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    }
   }
 };
 
